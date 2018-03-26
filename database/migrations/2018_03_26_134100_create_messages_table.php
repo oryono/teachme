9cc17503d6a534ca9c33e915ee35fb840919e801
Patrick Oryono <patricken08@gmail.com>
2018-03-26 16:52:51 +0300

    added models for messaging and notifications

---
+++ database/migrations/2018_03_26_134100_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('initiator')->unsigned();
+            $table->integer('recipient');
+            $table->text('message');
+            $table->boolean('seen')->default(0);
+            $table->boolean('delivered')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
